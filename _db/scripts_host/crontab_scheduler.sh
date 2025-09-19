783f372964a5e3ca0f0d33281fe090f71c8adbec
silvajunior <justinojjsj@gmail.com>
2025-09-19 20:17:01 -0300

    Atualização do banco 2025_09_19-20_17_01

--- _db/scripts_host/crontab_scheduler.sh
+++ _db/scripts_host/crontab_scheduler.sh
@@ -1,3 +1,3 @@
-#Colocar no crontab
+#Colocar no crontab do usuario git configurado ssh
 
 56 19 * * * "/home/silvajunior/Área de trabalho/desenvolvimento/pi6-tempguardianV2/_db/scripts_host/backupDB.sh"
